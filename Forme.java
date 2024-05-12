95a74abdd22e166a8ae9c63e900458edc4352f42
MargauxBarr <169059077+MargauxBarr@users.noreply.github.com>
2024-05-12 14:38:14 +0200

    v2

--- Forme.java
+++ Forme.java
@@ -1,3 +1,5 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class Forme {
 	private Point[] points; 
@@ -13,5 +15,30 @@ public class Forme {
 	public void setPoints(Point[] points) {
 		this.points = points;
 	}
+
+	public void dessiner(GraphicsContext gc) {
+		if (points.length < 2) {
+			return; 
+		}
+
+		gc.beginPath();
+		gc.moveTo(points[0].getX(), points[0].getY());
+	
+		//relie chaque point avec le précédent
+		for (int i = 1; i < points.length; i++) {
+			gc.lineTo(points[i].getX(), points[i].getY());
+		}
+		//relie dernier point au premier 
+		gc.closePath();
+
+		gc.setFill(Color.GREEN); 
+		gc.fill();
+	
+		//contour
+		gc.setStroke(Color.BLACK); 
+		gc.setLineWidth(2); //épaisseur trait
+		gc.stroke();
+	}
+
 
 }
