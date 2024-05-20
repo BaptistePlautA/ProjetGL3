bf38076543f3c1cc7b2c31576a268a7f00263593
MargauxBarr <169059077+MargauxBarr@users.noreply.github.com>
2024-05-20 15:44:39 +0200

    modifs suite au chgmt de visibilité du tab

--- src/controleurs/MorphingSimpleHandler.java
+++ src/controleurs/MorphingSimpleHandler.java
@@ -1,19 +1,27 @@
+
 package controleurs;
 
+import javafx.scene.image.ImageView;
 import java.util.Map;
 import utilitaires.*;
+
+import java.io.File;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
 
+
 public class MorphingSimpleHandler implements EventHandler<ActionEvent> {
     private TextField champEtapes;
     private TextField champDelai;
+    private ImageView imageGauche;
+    private String imagePath;
     
-    public MorphingSimpleHandler(TextField champEtapes, TextField champDelai) {
+    public MorphingSimpleHandler(TextField champEtapes, TextField champDelai, ImageView imageGauche) {
         this.champEtapes = champEtapes;
         this.champDelai = champDelai;
+        this.imageGauche= imageGauche;
         }
 
     @Override
@@ -22,14 +30,43 @@ public class MorphingSimpleHandler implements EventHandler<ActionEvent> {
         int nbEtapes = Integer.parseInt(champEtapes.getText());
         int delai = Integer.parseInt(champDelai.getText());
         
-        System.out.println("Nombre d'etapes : " + nbEtapes + ", delai (ms) : " + delai);
+        videDossierFormeSimples();
         
-        while(nbEtapes>=0) {
+        javafx.scene.image.Image image = imageGauche.getImage();
+        if (image != null) {
+            String imagePath = image.getUrl();
+            
+            File file = new File(imagePath);
+            String cheminImage = file.getPath();
+            
+            this.imagePath = cheminImage.substring("file:\\".length());
+        }
+                
+        ImageM imageFondModifie = modifFondImage(new ImageM(imagePath));
+        
+        
+        colorPointsDeControle(imageFondModifie);
+        while(nbEtapes>0) {
         	calculEnsemblePointSuivant(nbEtapes);
-        	
+        	modifFondImage(imageFondModifie);
+        	colorPointsDeControle(imageFondModifie);
         	nbEtapes-=1;
         }
+        ConvertisseurGIF convertisseur = new ConvertisseurGIF();
+        convertisseur.convertirEnGif(delai);
         
+    }
+    private void videDossierFormeSimples() {
+    	String directoryPath = "./FormesSimples";
+        File directory = new File(directoryPath);
+        if (directory.exists() && directory.isDirectory()) {
+            File[] contents = directory.listFiles();
+            if (contents != null) {
+                for (File file : contents) {
+                    file.delete(); // Supprime le fichier ou le dossier
+                }
+            }
+        }
     }
     private void calculEnsemblePointSuivant(int nbEtapes) {
     	
@@ -38,7 +75,7 @@ public class MorphingSimpleHandler implements EventHandler<ActionEvent> {
             Point pointDebut = entry.getValue();
             Point pointFin = PointsControleHandler.pointsControleFin.get(key);
             
-            System.out.println(key+" : ("+pointDebut.getX()+","+pointDebut.getY()+")");
+            //System.out.println(key+" : ("+pointDebut.getX()+","+pointDebut.getY()+")");
             calculPointSuivant(pointDebut, pointFin, nbEtapes);
         }
     }
@@ -63,4 +100,131 @@ public class MorphingSimpleHandler implements EventHandler<ActionEvent> {
         	pointDebut.setY(pointDebut.getY()-retraitY);
         }
     }
+    private ImageM modifFondImage(ImageM imageMGauche) {
+        Pixel[][] tab = imageMGauche.getTab();
+        for (int y = 0; y < imageMGauche.getLargeur(); y++) {
+            for (int x = 0; x < imageMGauche.getHauteur(); x++) {
+                
+            	tab[x][y].setR(255);
+            	tab[x][y].setV(255);
+            	tab[x][y].setB(255);
+            }
+        }
+        return imageMGauche;
+    }
+    private void colorPointsDeControle(ImageM imageMGauche) {
+        Pixel[][] tab = imageMGauche.getTab(); 
+    	ImageM imageModifiee = new ImageM(tab);
+    	
+    	Point pointDepart = null;
+    	int tailleMap = PointsControleHandler.pointsControleDebut.size();
+    	int pointEnCours = 1;
+    	    	
+    	for (Map.Entry<Character, Point> entry : PointsControleHandler.pointsControleDebut.entrySet()) {
+        	Character key = entry.getKey();
+            Point pointDebut = entry.getValue();
+            
+            //colore le point de contrôle en noir
+            tab[(int) pointDebut.getX()][(int) pointDebut.getY()].setR(0);
+            tab[(int) pointDebut.getX()][(int) pointDebut.getY()].setV(0);
+            tab[(int) pointDebut.getX()][(int) pointDebut.getY()].setB(0);
+            
+            if(key == 'A') {
+            	pointDepart = pointDebut;
+            }
+            
+            if(pointEnCours < tailleMap) {
+            	Character cleSuivante = (char) (key + 1);
+                Point pointSuivant = PointsControleHandler.pointsControleDebut.get(cleSuivante);
+                
+                drawLine(imageMGauche, pointDebut, pointSuivant);
+            }else {
+            	drawLine(imageMGauche, pointDebut, pointDepart);
+            }
+            
+            pointEnCours += 1;
+        }
+    	   	
+    	
+        remplirForme(imageMGauche);
+    	
+        String outputPath = "./FormesSimples/image_+"+System.currentTimeMillis()+".jpg";
+        imageModifiee.saveImage(outputPath);
+    }
+	private void drawLine(ImageM image, Point depart, Point arrivee) {
+        Pixel[][] tab = image.getTab();
+    	int x0 = (int) depart.getX();
+        int y0 = (int) depart.getY();
+        int x1 = (int) arrivee.getX();
+        int y1 = (int) arrivee.getY();
+
+        int dx = Math.abs(x1 - x0);
+        int dy = Math.abs(y1 - y0);
+        int sx = x0 < x1 ? 1 : -1;
+        int sy = y0 < y1 ? 1 : -1;
+        int err = dx - dy;
+
+        while (x0 != x1 || y0 != y1) {
+            // Colorer le pixel ici
+            tab[x0][y0].setR(0);
+            tab[x0][y0].setV(0);
+            tab[x0][y0].setB(0);
+
+            int err2 = 2 * err;
+            if (err2 > -dy) {
+                err -= dy;
+                x0 += sx;
+            }
+            if (err2 < dx) {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
+	private void remplirForme(ImageM image) {
+        Pixel[][] tab = image.getTab();
+    	for (int y = 0; y < image.getHauteur()-1; y++) {
+            for (int x = 0; x < image.getLargeur()-1; x++) {
+            	//si pixel noir, color la case a droite
+                if ((tab[x][y].getR() == 0) && (tab[x][y].getV() == 0) && (tab[x][y].getB() == 0))  {
+                	if ((tab[x+1][y].getR() == 255) && (tab[x+1][y].getV() == 255) && (tab[x+1][y].getB() == 255)) {
+                		tab[x+1][y].setR(0);
+                		tab[x+1][y].setV(255);
+                		tab[x+1][y].setB(0);
+                    }
+                	if ((tab[x-1][y].getR() == 0) && (tab[x-1][y].getV() == 255) && (tab[x-1][y].getB() == 0)
+                			&& (tab[x+1][y].getR() == 0) && (tab[x+1][y].getV() == 255) && (tab[x+1][y].getB() == 0)) {
+                		tab[x+1][y].setR(255);
+                		tab[x+1][y].setV(255);
+                		tab[x+1][y].setB(255);
+                    }
+                }
+                //pixel vert mais au dessus c'est blanc
+                if ((tab[x][y].getR() == 0) && (tab[x][y].getV() == 255) && (tab[x][y].getB() == 0)) {
+                	if ((tab[x][y-1].getR() == 255) && (tab[x][y-1].getV() == 255) && (tab[x][y-1].getB() == 255)) {
+                		tab[x][y].setR(255);
+                		tab[x][y].setV(255);
+                		tab[x][y].setB(255);
+                    }
+                }
+                //si pixel vert, color la case de droite
+                if ((tab[x][y].getR() == 0) && (tab[x][y].getV() == 255) && (tab[x][y].getB() == 0)) {
+                	//si case blanche a droite
+                	if ((tab[x+1][y].getR() == 255) && (tab[x+1][y].getV() == 255) && (tab[x+1][y].getB() == 255)) {
+                		tab[x+1][y].setR(0);
+                		tab[x+1][y].setV(255);
+                		tab[x+1][y].setB(0);
+                    }
+                }
+                /*if ((tab[x][y].getR() == 0) && (tab[x][y].getV() == 255) && (tab[x][y].getB() == 0)) {
+                	if ((tab[x-1][y].getR() == 0) && (tab[x-1][y].getV() == 0) && (tab[x-1][y].getB() == 0)) {
+                		tab[x+1][y].setR(255);
+                		tab[x+1][y].setV(0);
+                		tab[x+1][y].setB(0);
+                    }
+                }*/
+            }
+        }
+    }
+
 }
