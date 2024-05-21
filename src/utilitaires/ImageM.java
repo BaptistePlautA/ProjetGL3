8b1b5bc71e9929a86ea548f0ffa7210031501e50
MargauxBarr <169059077+MargauxBarr@users.noreply.github.com>
2024-05-21 11:23:29 +0200

    Update ImageM.java

--- src/utilitaires/ImageM.java
+++ src/utilitaires/ImageM.java
@@ -64,7 +64,7 @@ public class ImageM {
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                Pixel pixel = tab[y][x];
+                Pixel pixel = tab[x][y];
                 int rgb = (pixel.getR() << 16) | (pixel.getV() << 8) | pixel.getB();
                 image.setRGB(x, y, rgb);
             }
