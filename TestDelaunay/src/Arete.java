c5036d600b7053b9aab34d178a4ddbaef942e378
Baptiste Plaut-Aubry <121192282+BaptistePlautA@users.noreply.github.com>
2024-05-24 11:23:32 +0200

    Update Arete.java
    Avec Morphing fonctionnel

--- TestDelaunay/src/Arete.java
+++ TestDelaunay/src/Arete.java
@@ -1,15 +1,40 @@
+import java.util.Objects;
+
+//CLASSE ARETE : OK
 public class Arete {
-    Point p0;
-    Point p1;
+    private Point p0;
+    private Point p1;
 
     public Arete(Point p0, Point p1) {
         this.p0 = p0;
         this.p1 = p1;
     }
     
+    public Point getP0() {
+        return p0;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+    
+    public boolean contient(Point p) {
+        return p.equals(p0) || p.equals(p1);
+    }
+    
     //Note à moi même: bien écrire le equals
     //sous peine d'avoir des résultats complètement FAUX :))
     boolean equals(Arete e) {
         return (this.p0.equals(e.p0) && this.p1.equals(e.p1)) || (this.p0.equals(e.p1) && this.p1.equals(e.p0));
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(p0) + Objects.hash(p1);
+    }
+    
+    @Override
+    public String toString() {
+        return "Arete(" +"p0=" + p0 +", p1=" + p1 +')';
     }
 }
