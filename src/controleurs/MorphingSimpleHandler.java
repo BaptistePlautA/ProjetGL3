9b9ea7431187a56731553a598e49f543c725f78f
MargauxBarr <169059077+MargauxBarr@users.noreply.github.com>
2024-05-28 09:40:06 +0200

    v3

--- src/controleurs/MorphingSimpleHandler.java
+++ src/controleurs/MorphingSimpleHandler.java
@@ -4,30 +4,29 @@ package controleurs;
 import javafx.scene.image.ImageView;
 import utilitaires.*;
 import java.io.File;
+import java.util.Map;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
 
 public class MorphingSimpleHandler extends MorphingAbstract implements EventHandler<ActionEvent> {
-    private ImageView imageGauche;
-    private String imagePath;
     private PointsControleHandler handler; 
-    
+
     public MorphingSimpleHandler(TextField champEtapes, TextField champDelai, ImageView imageGauche, PointsControleHandler handler) {
-        super(champEtapes, champDelai); 
-        this.imageGauche= imageGauche;
-        this.handler = handler;
+        super(champEtapes, champDelai, imageGauche); 
+        this.handler = handler; 
     }
 
     @Override
     public void handle(ActionEvent event) {
-
+        long tempsDépart = System.currentTimeMillis();
         int nbEtapes = Integer.parseInt(getChampEtapes().getText());
         int delai = Integer.parseInt(getChampDelai().getText());
         
         dossierFormeSimples();
         
-        javafx.scene.image.Image image = imageGauche.getImage();
+        javafx.scene.image.Image image = getImageGauche().getImage();
         
         //si image non nulle, recupere le chemin de l'image et le stocke (en enlevant le début de la chaine 'file:\\'
         if (image != null) {
@@ -35,12 +34,21 @@ public class MorphingSimpleHandler extends MorphingAbstract implements EventHand
             
             File file = new File(imagePath);
             String cheminImage = file.getPath();
-            
-            this.imagePath = cheminImage.substring("file:\\".length());
+        
+            setImagePath(cheminImage.substring("file:\\".length()));
+        }
+
+        Map<Character, Point> pointsControleDebut = PointsControleHandler.getPointsControleFin();
+
+        // Afficher chaque élément de la map
+        for (Map.Entry<Character, Point> entry : pointsControleDebut.entrySet()) {
+            Character key = entry.getKey();
+            Point value = entry.getValue();
+            System.out.println("Key: " + key + ", Value: " + value.toString());
         }
         
         //creer le tableau de pixel de l'image et unifie son fond et le stocke
-        ImageM imageFondModifie = modifFondImage(new ImageM(imagePath));
+        ImageM imageFondModifie = modifFondImage(new ImageM(getImagePath()));
         
         //colore les points de contrôle de début, trace les droites entre ceux-ci et colore
         colorPointsDeControle(imageFondModifie, PointsControleHandler.getPointsControleDebut());
@@ -52,6 +60,11 @@ public class MorphingSimpleHandler extends MorphingAbstract implements EventHand
         	colorPointsDeControle(imageFondModifie, PointsControleHandler.getPointsControleDebut());
         	nbEtapes-=1;
         }
+        long tempsFin = System.currentTimeMillis();
+        
+        //calcul temps morphing en secondes 
+        double tempsMorphing = (tempsFin - tempsDépart) / 1000.0;
+        System.out.println("Temps de morphing : " + tempsMorphing + " s");
         
         //convertie les images en gif
         ConvertisseurGIF convertisseur = new ConvertisseurGIF();
